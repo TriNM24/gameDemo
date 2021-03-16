170968d2a0aaa06317f01c4077778fe1a31ebefd
nguyenminhtri <tri.nguyen833@gmail.com>
2021-03-16 09:36:58 +0700

    fix bug crash app in show loading dialog

--- DuyBinhGame/app/src/main/java/binh/le/game/login/LoginActivity.java
+++ DuyBinhGame/app/src/main/java/binh/le/game/login/LoginActivity.java
@@ -67,7 +67,6 @@ public class LoginActivity extends BaseActivity<ActivityLoginBinding> {
                         processResult(authResultTask);
                         if (loadingDialog != null) {
                             loadingDialog.dismissAllowingStateLoss();
-                            loadingDialog = null;
                         }
                     });
         } else {
